b2aae0ea661a65b7bebdd2004abcf5c6c9e95790
the-feimao <thefeimao.ng@gmail.com>
2026-01-26 21:24:59 +0100

    fixed expo db link from physical device

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/register', [AuthController::class,'register']);
-Route::post('/login', [AuthController::class,'login']);
+Route::post('/login', [AuthController::class,'login'])->name('login');
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
